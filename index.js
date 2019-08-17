8db12310e7a5131441cd568e8edbf26596f016c2
jackhao <tokyoandhao@gmail.com>
2019-08-17 09:22:51 +0700

    sayHi method for Person

---
+++ index.js
@@ -0,0 +1,5 @@
+var Person = require('./Person');
+
+var person1 = new Person('jackie');
+
+person1.sayHi();
--- person.js
+++ person.js
@@ -1,4 +1,11 @@
+var chalk = require('chalk')
+
 function Person(name){
-	this.name = Person;
-	
+	this.name = name;
 }
+
+Person.prototype.sayHi = function(){
+	console.log("My name is " + chalk.red(this.name))
+}
+
+module.exports = Person;
