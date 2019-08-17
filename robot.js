fb9a9e00171116f8e3dceec9aa2759f64589df7e
jackhao <tokyoandhao@gmail.com>
2019-08-17 11:04:04 +0700

    Improve the feature of Robot

--- robot.js
+++ robot.js
@@ -1,10 +1,22 @@
+var Rocket = require('./Rocket');
+
 function Robot(name){
 	this.name = name
 	this.auto = true;
+	this.machine = [];
+
 }
 
 Robot.prototype.auto = function() {
 	this.auto = false;
 }
 
-module.exports = Robot
+Robot.prototype.control = function(rocket){
+	if( rocket instanceof Rocket){
+		this.machine = [rocket];
+	} else {
+		throw new Error('Robot just can only control the machine')
+	}
+}
+
+module.exports = Robot;
